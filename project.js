996675c1a16404789e3e2622a07193960db364ad
winn-tech <olugodwin99@gmail.com>
2022-07-21 15:29:08 +0100

    first push

---
+++ project.js
@@ -0,0 +1,13 @@
+// function activeBtn(bttn){
+//     var bttn = document.getElementsByClassName("bttn")
+//     // for(i = 0; i < bttn.length; i++){
+//     //     bttn[i].style.backgroundColor = "red";
+//     // }
+//     bttn.style.backgroundColor = "red";
+// }
+
+
+function changeBgColor(play){
+    var source = document.getElementById("class").getAttribute("class");
+    var rpl = document.getElementsByClassName
+}
